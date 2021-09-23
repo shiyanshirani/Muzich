40d5c43776c6b44b61c6520d8d393b6682dc3fd3
shiyanshirani <shiyan99s@gmail.com>
2021-09-23 16:02:48 +0530

    generate new code added in code field without ()

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-import string, random
+import string
+import random
+
 
 def generate_unique_code():
     length = 6
@@ -11,10 +13,10 @@ def generate_unique_code():
     return code
 
 
-
 # Create your models here.
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=True)
+    code = models.CharField(
+        max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)

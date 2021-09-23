82a71c338178d351989476a6cb9a5d8d3f0c0fd2
shiyanshirani <shiyan99s@gmail.com>
2021-09-23 16:03:17 +0530

    CreateRoomSerializer added to get data from frontend

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import *
 
+
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = '__all__'
+
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')

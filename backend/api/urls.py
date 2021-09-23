23d43c7a852a8c55a6fe0a2161343bb45e0cdea3
shiyanshirani <shiyan99s@gmail.com>
2021-09-23 16:03:42 +0530

    room and create_room path added

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path 
-from .views import RoomView
+from django.urls import path
+from .views import CreateRoomView, RoomView
 
 
 urlpatterns = [
-    path('room/', RoomView.as_view() , name='room_view_set')
+    path('room', RoomView.as_view(), name='room_view_set'),
+    path('create_room', CreateRoomView.as_view(), name='create_room_view'),
 ]

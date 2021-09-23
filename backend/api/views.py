00f21edd1b1c297940793f84fb26898e96f5e90d
shiyanshirani <shiyan99s@gmail.com>
2021-09-23 16:06:00 +0530

    CreateRoomView APIView added for getting data from frontend

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,41 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import *
 from .serializers import *
 # Create your views here.
 
 
-
-class RoomView(generics.ListCreateAPIView):
+class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get('guest_can_pause')
+            votes_to_skip = serializer.data.get('votes_to_skip')
+            host = self.request.session.session_key
+            query_set = Room.objects.filter(host=host)
+            if query_set.exists():
+                room = query_set[0]
+                room.guest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+            else:
+                room = Room(host=host, guest_can_pause=guest_can_pause,
+                            votes_to_skip=votes_to_skip)
+                room.save()
+
+            return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'Bad Request': 'Invalid Data'}, status=status.HTTP_400_BAD_REQUEST)

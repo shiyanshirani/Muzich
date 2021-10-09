2d65e91bf3d3a21a277bde763dac2865c922a9a5
shiyanshirani <shiyan99s@gmail.com>
2021-10-09 23:18:18 +0530

    improper views

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -9,6 +9,6 @@ class RoomSerializer(serializers.ModelSerializer):
 
 
 class CreateRoomSerializer(serializers.ModelSerializer):
-    # class Meta:
-    #     model = Room
-    #     fields = ("guest_can_pause", "votes_to_skip")
+    class Meta:
+        model = Room
+        fields = ("guest_can_pause", "votes_to_skip")
